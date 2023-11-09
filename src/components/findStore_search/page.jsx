5aec2d9fd66678c85893986764be86e035934ae7
utkarshyadav11 <utkarshyadav6745@gmail.com>
2023-11-09 14:47:33 +0530

    new fix

--- src/components/findStore_search/page.jsx
+++ src/components/findStore_search/page.jsx
@@ -345,15 +345,9 @@ function Mapsection() {
                     </div>
                 </div>
                 <div className={styles.content70}>
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14687.12984113929!2d72.4599995347463!3d23.03175831407262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e9b9e585cfc57%3A0x833b59aeb8ec9afe!2sCrown%20Decor%20Private%20Limited!5e0!3m2!1en!2sin!4v1698659550023!5m2!1sen!2sin"
-                        allowFullScreen=""
-                        loading="lazy"
-                        width="100%" height="720"
-                        referrerPolicy="no-referrer-when-downgrade"
-                        className={styles.map_frame}
-                        title="Crown Decor Private Limited Location"
-                    ></iframe>
+
+                <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.425164494458!2d72.5118402758819!3d23.044869315494672!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e9b51fe5be8d9%3A0x1c4999478aa4081a!2sRoyale%20Touche%20Laminates%20and%20Wooden%20Floors%20-%20Ahmedabad!5e0!3m2!1sen!2sin!4v1699507206577!5m2!1sen!2sin" width="100%" height="720"  allowfullscreen="" className={styles.map_frame} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    
                 </div>
             </div>
         </div>

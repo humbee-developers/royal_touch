84b212e33b4a74f0095edf9dfa405fd89f042548
utkarshyadav11 <utkarshyadav6745@gmail.com>
2023-11-09 23:01:11 +0530

    happy diwali 2

--- src/components/product_overview/index.jsx
+++ src/components/product_overview/index.jsx
@@ -1,5 +1,20 @@
-import styles from "@/components/product_overview/product_overview.module.css"
-import Image from "next/image"
+// ProductOverviewItem.jsx
+import React, { useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import Image from 'next/image';
+import styles from '@/components/product_overview/product_overview.module.css';
+
+// Animation variants for the content
+const contentVariants = {
+  hidden: { opacity: 0, y: 50 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.8, ease: "easeOut" },
+  },
+};
+
 const ProductOverviewItem = ({
   heading,
   image,
@@ -7,27 +22,59 @@ const ProductOverviewItem = ({
   content,
   secondContent,
 }) => {
+  const controlsFirstContent = useAnimation();
+  const controlsSecondContent = useAnimation();
+  const [refFirstContent, inViewFirstContent] = useInView({ triggerOnce: true, rootMargin: '-100px 0px' });
+  const [refSecondContent, inViewSecondContent] = useInView({ triggerOnce: true, rootMargin: '-100px 0px' });
+
+  useEffect(() => {
+    if (inViewFirstContent) {
+      controlsFirstContent.start('visible').then(() => {
+        // Start the second content animation with a slight delay after the first content animation
+        setTimeout(() => {
+          controlsSecondContent.start('visible');
+        }, 0.2 * 1000); // 0.2 seconds delay
+      });
+    }
+  }, [controlsFirstContent, inViewFirstContent, controlsSecondContent]);
+
+  useEffect(() => {
+    if (inViewSecondContent && !inViewFirstContent) {
+      controlsSecondContent.start('visible');
+    }
+  }, [controlsSecondContent, inViewSecondContent, inViewFirstContent]);
+
   return (
     <div className={styles.productOverviewCard}>
       <div className={styles.productOverviewCard_wrapper}>
-        <h2>{heading}</h2>
+        <h2 className={styles.heading}>{heading}</h2>
         <div className={styles.image}>
-        <Image  src={image} alt="Your Image"  />
+          <Image src={image} alt={title} width={500} height={300} />
         </div>
         <div className={styles.contentSec}>
           <div className={styles.title}>{title}</div>
-          <div className={styles.content}>{content}</div>
-          <div className={styles.secondConetnt}>{secondContent}</div>
+          <motion.div
+            ref={refFirstContent}
+            variants={contentVariants}
+            initial="hidden"
+            animate={controlsFirstContent}
+            className={styles.content}
+          >
+            {content}
+          </motion.div>
+          <motion.div
+            ref={refSecondContent}
+            variants={contentVariants}
+            initial="hidden"
+            animate={controlsSecondContent}
+            className={styles.secondConetnt}
+          >
+            {secondContent}
+          </motion.div>
         </div>
       </div>
     </div>
   );
 };
+
 export default ProductOverviewItem;
-
-
-
-
-
-
-
